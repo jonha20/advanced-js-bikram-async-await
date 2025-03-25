cf7675a78ecef5a3053d3d2a29de8ee05e45945f
jonha20 <64472507+jonha20@users.noreply.github.com>
2025-03-25 15:42:04 +0100

    add/Ejercicios1-6

--- index.js
+++ index.js
@@ -1,11 +1,179 @@
 //DESARROLLA AQUI TUS SOLUCIONES
 
-async function getImageAndName (pokemon){
+//1.- Declara una función **getRandomPokemon** que retorne un pokemon aleatorio.
 
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    let data = await response.json();
-    let name = data.name;
-    let img = data.sprites.front_default;
-    return {name, img}
-    
+const getRandomPokemon = async () => {
+    let num = Math.floor(Math.random()*1302);
+  try {
+    // Realizar la solicitud a la API
+    let response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${num}`
+    );
+    // Verificar si la respuesta es exitosa
+    if (!response.ok) {
+      throw new Error(
+        `Error HTTP: ${response.status} - ${response.statusText}`
+      );
+    }
+
+    // Si la respuesta es exitosa, procesar los datos
+    const data = await response.json();
+    return ("Pokemon: ", data);
+  } catch (error) {
+    // Manejar errores de red o del servidor
+    console.error("Hubo un problema con la solicitud:", error.message);
+  }
+};
+
+// Llamar a la función para hacer la solicitud
+console.log(getRandomPokemon());
+
+//2.- Declara una funcion **getImageAndName** que retorne el nombre y la URL de la imagen de un pokemon => (return {img, name})
+
+async function getImageAndName(pokemon) {
+try {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    if (!response.ok) {
+        throw new Error(
+          `Error HTTP: ${response.status} - ${response.statusText}`
+        );
+      }
+      let data = await response.json();
+      let name = data.name;
+      let img = data.sprites.front_default;
+      return { name, img }
+
+} catch (error) {
+    // Manejar errores de red o del servidor
+    console.error("Hubo un problema con la solicitud:", error.message);
+  }
 }
+console.log(getImageAndName("bulbasaur"))
+
+//3.- Declara una funcion **printImageAndName** que retorne el string necesario para pintar la imagen y el nombre del pokemon en el DOM
+
+async function getImageAndName() {
+    try {
+       let img =  getImageAndName("bulbasaur")
+       let pokemonName = getImageAndName("bulbasaur")
+       console.log(pokemonName);
+       
+    
+    } catch (error) {
+        // Manejar errores de red o del servidor
+        console.error("Hubo un problema con la solicitud:", error.message);
+      }
+    }
+    console.log(getImageAndName("bulbasaur"))
+
+//4.- Declara una función **getRandomDogImage** que retorne la url de la imagen de un perro aleatorio
+
+async function getRandomDogImage() { 
+    
+    try {
+        // Realizar la solicitud a la API
+        let response = await fetch(
+          `https://dog.ceo/api/breeds/image/random`
+        );
+        // Verificar si la respuesta es exitosa
+        if (!response.ok) {
+          throw new Error(
+            `Error HTTP: ${response.status} - ${response.statusText}`
+          );
+        }
+    
+        // Si la respuesta es exitosa, procesar los datos
+        const data = await response.json();
+        return data.message;
+      } catch (error) {
+        // Manejar errores de red o del servidor
+        console.error("Hubo un problema con la solicitud:", error.message);
+      }
+
+}
+console.log(getRandomDogImage());
+
+//5.- Declara una función **getRandomPokemonImage** que retorne la url de la imagen de un pokemon aleatorio.
+
+async function getRandomPokemonImage() { 
+    let num = Math.floor(Math.random()*1302);
+    try {
+        // Realizar la solicitud a la API
+        let response = await fetch(
+           `https://pokeapi.co/api/v2/pokemon/${num}`
+        );
+        // Verificar si la respuesta es exitosa
+        if (!response.ok) {
+          throw new Error(
+            `Error HTTP: ${response.status} - ${response.statusText}`
+          );
+        }
+    
+        // Si la respuesta es exitosa, procesar los datos
+        const data = await response.json();
+        return data.sprites.front_default;
+      } catch (error) {
+        // Manejar errores de red o del servidor
+        console.error("Hubo un problema con la solicitud:", error.message);
+      }
+
+}
+console.log(getRandomPokemonImage());
+
+
+
+//7.- Declara una función **getRandomCharacter** que retorne un personaje aleatorio.
+
+async function getRandomCharacter() { 
+    let num = Math.floor(Math.random()*20);
+    try {
+        // Realizar la solicitud a la API
+        let response = await fetch(
+           `https://rickandmortyapi.com/api/character/${num}`
+        );
+        // Verificar si la respuesta es exitosa
+        if (!response.ok) {
+          throw new Error(
+            `Error HTTP: ${response.status} - ${response.statusText}`
+          );
+        }
+    
+        // Si la respuesta es exitosa, procesar los datos
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        // Manejar errores de red o del servidor
+        console.error("Hubo un problema con la solicitud:", error.message);
+      }
+
+}
+console.log(getRandomCharacter());
+
+//8.- Declara una función **getRandomCharacterInfo** que retorne de un personaje su imagen, nombre, episodios en los que aparece y el nombre del primer episodio en el que aparece + fecha de estreno
+
+async function getRandomCharacterInfo(num) { 
+    try {
+        // Realizar la solicitud a la API
+        let response = await fetch(
+           `https://rickandmortyapi.com/api/character/${num}`
+        );
+        // Verificar si la respuesta es exitosa
+        if (!response.ok) {
+          throw new Error(
+            `Error HTTP: ${response.status} - ${response.statusText}`
+          );
+        }
+    
+        // Si la respuesta es exitosa, procesar los datos
+        const data = await response.json();
+        let name = data.name;
+        let img = data.image;
+        let episode = data.episode;
+        return{name,img,episode}
+      } catch (error) {
+        // Manejar errores de red o del servidor
+        console.error("Hubo un problema con la solicitud:", error.message);
+      }
+
+}
+console.log(getRandomCharacterInfo(5));
